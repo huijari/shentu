3f56bca5cbb0b9c1e1a03fa4a9f50d8dd93b1f0c
Alexandre Cesar <acmorcego@gmail.com>
2018-09-05 11:05:13 -0300

    read meta

---
+++ src/meta.js
@@ -0,0 +1,23 @@
+const { parse } = require('toml')
+const { readFileSync } = require('fs')
+const ow = require('ow')
+
+function validate({ salt, timeout, services }) {
+  ow(salt, ow.string.label('salt').nonEmpty)
+  ow(timeout, ow.number.label('timeout').positive.integer)
+  ow(services, ow.array.label('service'))
+  for ({ name, version, length } of services) {
+    ow(name, ow.string.label('name').nonEmpty)
+    ow(version, ow.number.label('version').positive.integer)
+    ow(length, ow.number.label('length').integer.greaterThan(0))
+  }
+}
+
+function readMeta(path) {
+  const { salt, timeout, service: services } = parse(readFileSync(path))
+  const data = { salt, timeout, services }
+  validate(data)
+  return data
+}
+
+module.exports = { readMeta }
